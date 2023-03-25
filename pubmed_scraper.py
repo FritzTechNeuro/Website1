8b43a990492aee3b05a8527b654d6eb33d91363c
FritzTechNeuro <128919603+FritzTechNeuro@users.noreply.github.com>
2023-03-25 14:20:37 -0400

    Update pubmed_scraper.py

--- pubmed_scraper.py
+++ pubmed_scraper.py
@@ -1,18 +1,26 @@
 <?php
 
 // Define the PubMed API endpoint
-$pubmed_url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi';
+$pubmed_url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi';
 
 // Set the search parameters
 $search_term = 'neuroscience';
 $search_database = 'pubmed';
-$search_retmax = 5;
+$search_retmax = 1;
+
+// Calculate the publication date range for the last week
+$start_date = date('Y/m/d', strtotime('-1 week'));
+$end_date = date('Y/m/d');
 
 // Build the query string
 $query_string = http_build_query([
     'db' => $search_database,
     'term' => $search_term,
+    'datetype' => 'pdat',
+    'mindate' => $start_date,
+    'maxdate' => $end_date,
     'retmax' => $search_retmax,
+    'sort' => 'pubdate',
 ]);
 
 // Send the request to PubMed API
@@ -24,45 +32,18 @@ curl_close($curl);
 
 // Parse the response XML
 $xml = simplexml_load_string($response);
-$id_list = (array) $xml->IdList->Id;
+$docsum = $xml->DocSum;
 
-// Define the PubMed API endpoint for retrieving paper details
-$pubmed_fetch_url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi';
+// Extract the paper details
+$title = (string) $docsum->Item[1]->value;
+$authors = (string) $docsum->Item[2]->value;
+$journal = (string) $docsum->Item[3]->value;
+$citations = (int) $docsum->Item[4]->value;
 
-// Generate HTML code for the papers
-$html = '<ul>';
-foreach ($id_list as $id) {
-    // Build the query string
-    $query_string = http_build_query([
-        'db' => $search_database,
-        'id' => $id,
-        'retmode' => 'xml',
-    ]);
-
-    // Send the request to PubMed API
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, "{$pubmed_fetch_url}?{$query_string}");
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    $response = curl_exec($curl);
-    curl_close($curl);
-
-    // Parse the response XML
-    $xml = simplexml_load_string($response);
-
-    // Extract the paper details
-    $title = (string) $xml->PubmedArticle->MedlineCitation->Article->ArticleTitle;
-    $authors = array_map(function($author) {
-        return (string) $author->LastName . ' ' . (string) $author->Initials;
-    }, (array) $xml->PubmedArticle->MedlineCitation->Article->AuthorList->Author);
-    $journal = (string) $xml->PubmedArticle->MedlineCitation->Article->Journal->Title;
-    $doi = (string) $xml->PubmedArticle->PubmedData->ArticleIdList->ArticleId[1];
-
-    // Generate the HTML code for the paper
-    $html .= "<li><strong>{$title}</strong><br>";
-    $html .= implode(', ', $authors) . "<br>";
-    $html .= "{$journal} {$doi}</li>";
-}
-$html .= '</ul>';
+// Generate the HTML code for the paper
+$html = "<ul><li><strong>{$title}</strong><br>";
+$html .= "{$authors}<br>";
+$html .= "{$journal} ({$citations} citations)</li></ul>";
 
 // Print the HTML code
 echo $html;
