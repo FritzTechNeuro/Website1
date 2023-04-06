0fd540579dbb2ac26fdd43cdb344ee1afbc1db48
FritzTechNeuro <128919603+FritzTechNeuro@users.noreply.github.com>
2023-04-05 21:41:52 -0400

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $subject = $_POST["subject"];
+  $message = $_POST["message"];
+  
+  $to = "youremail@example.com"; // replace with your email address
+  $subject = "New message from your website";
+  $body = "Name: $name\nEmail: $email\nSubject: $subject\nMessage: $message";
+  
+  if (mail($to, $subject, $body)) {
+    echo "Thank you for your message!";
+  } else {
+    echo "Sorry, there was an error sending your message. Please try again later.";
+  }
+}
+?>
